3e4bceea989c7b2b784eb6713c0246ae595d4d51
barrieumaru <umarujogor@hotmail.com>
2020-07-26 00:22:10 +0200

    few page of bagara

--- script.js
+++ script.js
@@ -10,18 +10,258 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
-// Music
+// Quran
 const songs = [
   {
-    name: 'fatiha01',
-    displayName: 'Fatiha-01',
+    name: '000_1',
+    displayName: 'Explanation of Bismillah',
     artist: 'Ustaz Kalla',
   },
   {
-    name: '2-bagara01-43',
-    displayName: 'Bagara 01-43',
+    name: '001_001',
+    displayName: 'Surat Fatiha',
     artist: 'Ustaz Kalla',
   },
+   {
+    name: '002_001',
+    displayName: 'Surat Baqara 1-4',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_005',
+    displayName: 'Surat Baqara 5-16',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_017',
+    displayName: 'Surat Baqara 17-23',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_024',
+    displayName: 'Surat Baqara 24-28',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_029',
+    displayName: 'Surat Baqara 29-36',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_037',
+    displayName: 'Surat Baqara 37-47',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_048',
+    displayName: 'Surat Baqara 48-56',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_057',
+    displayName: 'Surat Baqara 57-60',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_062',
+    displayName: 'Surat Baqara 61-68',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_070',
+    displayName: 'Surat Baqara 69-75',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_077',
+    displayName: 'Surat Baqara 78-82',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_084',
+    displayName: 'Surat Baqara 83-87',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_089',
+    displayName: 'Surat Baqara 88-92',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_094',
+    displayName: 'Surat Baqara 93-100',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_102',
+    displayName: 'Surat Baqara 101-104',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_106',
+    displayName: 'Surat Baqara 105-111',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_113',
+    displayName: 'Surat Baqara 112-118',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_120',
+    displayName: 'Surat Baqara 119-125',
+    artist: 'Ustaz Kalla',
+  },
+  {
+    name: '002_127',
+    displayName: 'Surat Baqara 126-133',
+    artist: 'Ustaz Kalla',
+  },
+  // {
+  //   name: '002_135',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_142',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_146',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_154',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_164',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_170',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_182',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_187',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_191',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_197',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_203',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_211',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_216',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_220',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_225',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_231',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_234',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_238',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_246',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_249',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_253',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_257',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_260',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_265',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_270',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_275',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_282',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '002_283',
+  //   displayName: 'Surat Baqara 6-',
+  //   artist: 'Ustaz Kalla',
+  // },
+  // {
+  //   name: '',
+  //   displayName: '',
+  //   artist: 'Ustaz Kalla',
+  // },
 ];
 
 // Check if Playing
